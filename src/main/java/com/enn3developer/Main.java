fd00379be5a8b3b2a2c485b250c8a8276910b5e2
Enn3Developer <arturpeshko39@gmail.com>
2022-05-23 16:38:47 +0200

    Changed window's title

--- src/main/java/com/enn3developer/Main.java
+++ src/main/java/com/enn3developer/Main.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         ReversePolishNotation.setupOperations();
-        JFrame frame = new JFrame("App");
+        JFrame frame = new JFrame("Calculator");
         frame.setContentPane(new App().contentPane);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
